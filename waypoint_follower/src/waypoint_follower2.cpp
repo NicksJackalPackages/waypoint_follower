4c73e95bd876b52d8482eadbf44d1896cb14f333
Nick-Sullivan <nick.dave.sullivan@gmail.com>
2018-06-08 14:17:27 +0930

    faster angle change

--- waypoint_follower/src/waypoint_follower2.cpp
+++ waypoint_follower/src/waypoint_follower2.cpp
@@ -5,8 +5,8 @@
 #include "geometry_msgs/PoseArray.h"
 #include "geometry_msgs/TransformStamped.h"
 #include "nav_msgs/Path.h"
-#include "waypoint_follower_msgs/Waypoint.h"
-#include "waypoint_follower_msgs/WaypointStamped.h"
+//#include "waypoint_follower_msgs/Waypoint.h"
+//#include "waypoint_follower_msgs/WaypointStamped.h"
 #include "waypoint_follower_msgs/WaypointArray.h"
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
@@ -16,7 +16,7 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <robot_localization/navsat_conversions.h>
 
-/* Written by Nick Sullivan, The University of Adelaide. 
+/* Written by Nick Sullivan, The University of Adelaide.
 
 */
 
@@ -26,10 +26,10 @@ using std::endl;
 using std::string;
 using geometry_msgs::PointStamped;
 using geometry_msgs::PoseStamped;
-using geometry_msgs::TransformStamped;   
-using geometry_msgs::Twist;         
-using waypoint_follower_msgs::Waypoint;
-using waypoint_follower_msgs::WaypointStamped;
+using geometry_msgs::TransformStamped;
+using geometry_msgs::Twist;
+//using waypoint_follower_msgs::Waypoint;
+//using waypoint_follower_msgs::WaypointStamped;
 using waypoint_follower_msgs::WaypointArray;
 
 WaypointArray waypoint_array;          //list of waypoints to complete
@@ -40,7 +40,7 @@ string map_frame;                      //"map"
 string odom_frame;                     //"name_space/odom"
 string baselink_frame;                 //"name_space/base_link"
 string launch_frame;				           //frame_id of launch file waypoints
-bool include_start_pose;               //include the start pose in path visualisation               
+bool include_start_pose;               //include the start pose in path visualisation
 bool enabled;
 
 PoseStamped start_pose;
@@ -57,12 +57,12 @@ tf2_ros::Buffer tfbuffer;            //for transformation lookup
 
 // Convert GPS coordinates into values relative to the UTM frame.
 PointStamped convertLatLongToUTM(double lati, double longi){
-  cout << "Converting: ( " << lati << ", " << longi << ")" << endl;
+  //cout << "Converting: ( " << lati << ", " << longi << ")" << endl;
   double utm_northing, utm_easting;
   string utm_zone;
   RobotLocalization::NavsatConversions::LLtoUTM(lati, longi, utm_northing, utm_easting, utm_zone);
-  cout << cout.precision(10) << "Results: ( " << utm_easting << ", " << utm_northing << ")" << endl;
-  cout << "zone: " << utm_zone << endl;
+  //cout << cout.precision(10) << "Results: ( " << utm_easting << ", " << utm_northing << ")" << endl;
+  //cout << "zone: " << utm_zone << endl;
   geometry_msgs::PointStamped ps;
   ps.header.frame_id = "utm";
   ps.header.stamp = ros::Time(0);
@@ -97,19 +97,19 @@ bool convertUTMtoMap( geometry_msgs::PointStamped &p ){
 bool convertPoseFrame( geometry_msgs::Pose &p, string frame_in, string frame_out ){
   try {
     tf::TransformListener listener;
-    cout << "Time is: " << ros::Time::now() << endl;
-    cout << "Looking up transform from " << frame_in << " to " << frame_out << endl;
+    //cout << "Time is: " << ros::Time::now() << endl;
+    //cout << "Looking up transform from " << frame_in << " to " << frame_out << endl;
     geometry_msgs::PoseStamped old_pose, new_pose;
     ros::Time now            = ros::Time(0);
     old_pose.pose            = p;
     old_pose.header.frame_id = frame_in;
     old_pose.header.stamp    = now;
-    
+
     listener.waitForTransform(frame_in, frame_out, now, ros::Duration(20) );
     listener.transformPose(frame_out, old_pose, new_pose);
-    
+
     p = new_pose.pose;
-    
+
     return true;
   } catch (tf2::TransformException e){
     cout << "Failed to lookup transform from " << frame_in << " to " << frame_out << endl;
@@ -121,12 +121,12 @@ bool convertPoseFrame( geometry_msgs::Pose &p, string frame_in, string frame_out
 // was successful.
 bool lookupTransform(TransformStamped &output, string frame1, string frame2, ros::Time t){
   try{
-    cout << "Trying to get from frame: " << frame1 << " to " << frame2 << endl;                           
+    //cout << "Trying to get from frame: " << frame1 << " to " << frame2 << endl;
     if (!tfbuffer.canTransform(frame1, frame2, t) ){
       ROS_WARN("Unable to obtain a TF in observing.cpp");
       return false;
     }
-    output = tfbuffer.lookupTransform(frame1, frame2, t);                                         
+    output = tfbuffer.lookupTransform(frame1, frame2, t);
   } catch (tf2::TransformException &ex) {
     ROS_WARN("%s",ex.what());
     return false;
@@ -138,7 +138,6 @@ bool lookupTransform(TransformStamped &output, string frame1, string frame2, ros
 /**************************************************
  * Publisher functions
  **************************************************/
- 
 
 void publishWaypointPaths(){
   //cout << "Publishing waypoint paths!" << endl;
@@ -182,15 +181,15 @@ void publishWaypointPaths(){
 /**************************************************
  * Looping
  **************************************************/
- 
+
 // Called repeatedly until all waypoints are completed.
 void loop(){
   publishWaypointPaths();
   if( !enabled || current_waypoint_index < 0) return;
   double x, y, wp_x, wp_y, wp_ang, qx, qy, qz, qw, roll, pitch, yaw, dx, dy, dist, ang;
-  
+
   // Grab our current location.
-  cout << "Getting current location. " << endl;
+  //cout << "Getting current location. " << endl;
   TransformStamped ts_map2base;
   if ( !lookupTransform(ts_map2base, map_frame, baselink_frame, ros::Time(0)) ) return;
   x  = ts_map2base.transform.translation.x;
@@ -202,28 +201,28 @@ void loop(){
   tf2::Quaternion quat(qx, qy, qz, qw);
   tf2::Matrix3x3 mat(quat);
   mat.getRPY(roll, pitch, yaw);
-  cout << "x, y, yaw: (" << x << " , " << y << " , " << yaw << ")" << endl;
-  
+  //cout << "x, y, yaw: (" << x << " , " << y << " , " << yaw << ")" << endl;
+
   // Grab the waypoint location.
-  cout << "current_waypoint_index: " << current_waypoint_index << endl;
+  //cout << "current_waypoint_index: " << current_waypoint_index << endl;
   PoseStamped ps = waypoint_array.waypoints[current_waypoint_index];
   // Calculate the distance from our location to our target location.
-  cout << "Getting destination waypoint. " << endl;
+  //cout << "Getting destination waypoint. " << endl;
   wp_x = ps.pose.position.x;
   wp_y = ps.pose.position.y;
   dx   = wp_x - x;
   dy   = wp_y - y;
   wp_ang = atan2( dy, dx );
-  cout << "x, y, yaw: (" << wp_x << " , " << wp_y <<  " , " << wp_ang << ")" << endl;
-  
+  //cout << "x, y, yaw: (" << wp_x << " , " << wp_y <<  " , " << wp_ang << ")" << endl;
+
   // Convert from x-y to dist-ang.
   dist = sqrt( dx*dx + dy*dy );
   ang  = wp_ang - yaw;
   if( ang >  3.1415 ) ang = ang - 2*3.1415;
   if( ang < -3.1415 ) ang = ang + 2*3.1415;
-  cout << "dx, dy: (" << dx << " , " << dy << ")" << endl;
-  cout << "dist, ang: (" << dist << " , " << ang << ")" << endl;
-  
+  //cout << "dx, dy: (" << dx << " , " << dy << ")" << endl;
+  //cout << "dist, ang: (" << dist << " , " << ang << ")" << endl;
+
   // Produce a command velocity.
   double x_vel, yaw_vel;
   x_vel = 0;
@@ -233,15 +232,15 @@ void loop(){
     current_waypoint_index++;
     if( current_waypoint_index >= waypoint_array.waypoints.size() ){
       enabled = false;
-      cout << "DONE" << endl;
+      cout << "WAYPOINTS DONE" << endl;
     }
   }
   yaw_vel = 0;
   double ang_abs = sqrt( ang*ang );
-  if( ang_abs > 0.2 ) {
-    yaw_vel = 1;
+  if( ang_abs > 0.5 ) {
+    yaw_vel = 2;
   } else {
-    yaw_vel = 5.0*ang_abs;
+    yaw_vel = 4.0*ang_abs;
   }
   if( ang < 0 ){
     yaw_vel = -yaw_vel;
@@ -249,20 +248,20 @@ void loop(){
   Twist cmd_vel;
   cmd_vel.linear.x  = x_vel;
   cmd_vel.angular.z = yaw_vel;
-  cout << "xlin, yawvel: (" << x_vel << " , " << yaw_vel << ")" << endl;
+  //cout << "xlin, yawvel: (" << x_vel << " , " << yaw_vel << ")" << endl;
   pub_cmd_vel.publish(cmd_vel);
 }
- 
+
 /**************************************************
  * Initialising functions
  **************************************************/
- 
+
 void loadSubs(ros::NodeHandle n){
-  //sub_add_waypoint     = n.subscribe("add_waypoint",      100, callbackAddWaypoint);   
+  //sub_add_waypoint     = n.subscribe("add_waypoint",      100, callbackAddWaypoint);
   //sub_set_waypoints    = n.subscribe("set_waypoints",     100, callbackSetWaypoints);
-  //sub_upd_waypoints    = n.subscribe("update_waypoints",  100, callbackUpdateWaypoints);                                       
-  //sub_obs_waypoint     = n.subscribe("observed_waypoints",100, callbackObservedWaypoints);  
-  //sub_move_base_result = n.subscribe("move_base/result",  100, callbackMoveBaseResult);  
+  //sub_upd_waypoints    = n.subscribe("update_waypoints",  100, callbackUpdateWaypoints);
+  //sub_obs_waypoint     = n.subscribe("observed_waypoints",100, callbackObservedWaypoints);
+  //sub_move_base_result = n.subscribe("move_base/result",  100, callbackMoveBaseResult);
 }
 
 void loadPubs(ros::NodeHandle n){
@@ -276,6 +275,8 @@ void loadPubs(ros::NodeHandle n){
 void loadParams(ros::NodeHandle n_priv){
   vector<double> waypoint_values;    //position (x,y,z) and orientation (x,y,z,w)
   vector<double> gps_values;         //position (lat, long)
+  vector<int>    waypoint_order;     //indices of waypoint_values
+  vector<int>    gps_waypoint_order; //indices of gps_values
   enabled = true;
   // Set default parameters.
   double default_waypoint_values[] = {};
@@ -284,15 +285,28 @@ void loadParams(ros::NodeHandle n_priv){
   string default_map_frame       = "map";
   string default_odom_frame      = "odom";
   bool default_include_start_pose = true;
-  
+
   n_priv.param("baselink_frame",     baselink_frame,     default_baselink_frame);
   n_priv.param("map_frame",          map_frame,          default_map_frame);
   n_priv.param("odom_frame",         odom_frame,         default_odom_frame);
   n_priv.param("launch_frame",       launch_frame,       odom_frame);
   n_priv.param("include_start_pose", include_start_pose, default_include_start_pose);
-  
-  // Check parameter server to override defaults.
+
+  // Get waypoint order.
   XmlRpc::XmlRpcValue v;
+  if( n_priv.getParam("waypoint_order", v) ){
+    ROS_ASSERT(v.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    for(int i=0; i < v.size(); i++)  {
+      if( v[i].getType() == XmlRpc::XmlRpcValue::TypeDouble ) {
+        waypoint_order.push_back(v[i]);
+      }
+      if( v[i].getType() == XmlRpc::XmlRpcValue::TypeInt ) {
+        int d = v[i];
+        waypoint_order.push_back(d);
+      }
+    }
+  }
+  // Get waypoints.
   if( n_priv.getParam("waypoints", v) ){
     ROS_ASSERT(v.getType() == XmlRpc::XmlRpcValue::TypeArray);
     for(int i=0; i < v.size(); i++)  {
@@ -305,26 +319,47 @@ void loadParams(ros::NodeHandle n_priv){
       }
     }
   } else {
-    waypoint_values.assign(default_waypoint_values, default_waypoint_values + 
+    waypoint_values.assign(default_waypoint_values, default_waypoint_values +
                       sizeof(default_waypoint_values) / sizeof(default_waypoint_values[0]) );
   }
-  
+
   // Convert waypoint values into waypoints.
   if( waypoint_values.size() % 2 != 0 ){
     cout << "INCORRECT NUMBER OF WAYPOINT VALUES" << endl;
     return;
   }
-  for( int i=0; i<waypoint_values.size(); i+=2 ) {
+  // Fill in order (starts with 1).
+  if( waypoint_order.empty() ){
+    while( waypoint_order.size() < waypoint_values.size()/2 ){
+      waypoint_order.push_back( waypoint_order.size()+1 );
+    }
+  }
+  for( int i=0; i<waypoint_order.size(); i++ ) {
+    int j = (waypoint_order[i]-1)*2;
     PoseStamped ps;
     ps.header.seq      = 0;
     ps.header.stamp    = ros::Time::now();
     ps.header.frame_id = launch_frame;
-    ps.pose.position.x    = waypoint_values[i];
-    ps.pose.position.y    = waypoint_values[i+1];
+    ps.pose.position.x    = waypoint_values[j];
+    ps.pose.position.y    = waypoint_values[j+1];
     ps.pose.orientation.w = 1;
     waypoint_array.waypoints.push_back(ps);
+    cout << "wp: " << ps.pose.position.x << " , " << ps.pose.position.y << endl;
   }
-  
+
+  // Get GPS order.
+  if( n_priv.getParam("gps_waypoint_order", v) ){
+    ROS_ASSERT(v.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    for(int i=0; i < v.size(); i++)  {
+      if( v[i].getType() == XmlRpc::XmlRpcValue::TypeDouble ) {
+        gps_waypoint_order.push_back(v[i]);
+      }
+      if( v[i].getType() == XmlRpc::XmlRpcValue::TypeInt ) {
+        int d = v[i];
+        gps_waypoint_order.push_back(d);
+      }
+    }
+  }
   // Get GPS locations.
   if( n_priv.getParam("gps_waypoints", v) ){
     ROS_ASSERT(v.getType() == XmlRpc::XmlRpcValue::TypeArray);
@@ -338,7 +373,7 @@ void loadParams(ros::NodeHandle n_priv){
       }
     }
   } else {
-    gps_values.assign(default_gps_values, default_gps_values + 
+    gps_values.assign(default_gps_values, default_gps_values +
                       sizeof(default_gps_values) / sizeof(default_gps_values[0]) );
   }
   // Convert gps values into waypoints, store them relative to the map_frame.
@@ -346,15 +381,22 @@ void loadParams(ros::NodeHandle n_priv){
     cout << "INCORRECT NUMBER OF GPS WAYPOINT VALUES" << endl;
     return;
   }
+  // Fill in default order (starts with 1).
+  if( waypoint_order.empty() ){
+    while( gps_waypoint_order.size() < gps_values.size()/2 ){
+      gps_waypoint_order.push_back( gps_waypoint_order.size()+1 );
+    }
+  }
   cout << "NUMBER OF GPS WAYPOINTS: " << gps_values.size()/2 << endl;
-  for( int i=0; i<gps_values.size(); i+=2 ) {
+  for( int i=0; i<gps_waypoint_order.size(); i++ ) {
+    int j = (waypoint_order[i]-1)*2;
     cout << "FIRST GPS PAIR LOADED" << endl;
     PoseStamped ws;
-    double lati  = gps_values[i];
-    double longi = gps_values[i+1];
+    double lati  = gps_values[j];
+    double longi = gps_values[j+1];
     PointStamped ps = convertLatLongToUTM(lati, longi);
     convertUTMtoMap( ps );
-    
+
     ws.header = ps.header;
     ws.pose.position.x = ps.point.x;
     ws.pose.position.y = ps.point.y;
@@ -376,18 +418,16 @@ int main(int argc, char** argv){
   loadPubs(n);
   loadParams(n_priv);
   tf2_ros::TransformListener tflistener(tfbuffer);   //must stay persistent
-  
-  current_waypoint_index = 1;
-  
+
+  current_waypoint_index = 0;
+
   ros::Rate loop_rate(10);
   while( ros::ok() ){
     loop();
-    
+
     ros::spinOnce();
     loop_rate.sleep();
   }
-  
+
   return 0;
 };
-
-
